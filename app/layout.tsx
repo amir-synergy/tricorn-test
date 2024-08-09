ee7613be853e08573fc4fc4cb5a75d82466be03a
AmirkhonMakhkamov <100461361+AmirkhonMakhkamov@users.noreply.github.com>
2024-08-09 12:03:11 -0400

    meta-tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
-import Head from 'next/head';
 import "./custom.css";
 import SessionWrapper from "@/app/SessionWrapper";
 
@@ -14,7 +13,7 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-    title: "Team Signal",
+    title: "Dashboard | Signal",
     description: "Mobile Patrol Security Services",
 };
 
@@ -25,22 +24,13 @@ export default async function RootLayout({
 }) {
     return (
         <html lang="en">
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="application-name" content="Team Signal" />
-            <meta name="apple-mobile-web-app-title" content="Team Signal" />
-            <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta name="mobile-web-app-capable" content="yes" />
-            <meta name="apple-mobile-web-app-status-bar-style" content="white" />
-            <link rel="apple-touch-icon" href="/logo/logo.png" />
-        </Head>
-        <body className={montserrat.className}>
-        <SessionWrapper>
-            <Theme accentColor='blue'>
-                {children}
-            </Theme>
-        </SessionWrapper>
-        </body>
+            <body className={montserrat.className}>
+                <SessionWrapper>
+                    <Theme accentColor='blue'>
+                        {children}
+                    </Theme>
+                </SessionWrapper>
+            </body>
         </html>
     );
 }
