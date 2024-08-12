ac3ed390ec71b6bf6618e804372b4819c09e933b
AmirkhonMakhkamov <100461361+AmirkhonMakhkamov@users.noreply.github.com>
2024-08-12 14:19:34 -0400

    1.0.41

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Montserrat } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import "./custom.css";
 import SessionWrapper from "@/app/SessionWrapper";
+import Head from "next/head";
 
 const montserrat = Montserrat({
     subsets: ["latin"],
@@ -24,6 +25,9 @@ export default async function RootLayout({
 }) {
     return (
         <html lang="en">
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+            </Head>
             <body className={montserrat.className}>
                 <SessionWrapper>
                     <Theme accentColor='blue'>
